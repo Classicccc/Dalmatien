e4a90547bd01f362d264fa266df724e17fdc1ed6
Classicccc <alex.artemjev2012@yandex.ru>
2020-11-15 18:58:11 +0300

    much better. upgrade loading

--- frontend/posts.js
+++ frontend/posts.js
@@ -6,7 +6,10 @@ let timerId2 = setInterval(async () => {
 
 async function loadPosts()
 {
-    document.getElementsByClassName("posts")[0].innerHTML = "Update your posts..."
+    document.getElementsByClassName("posts")[0].innerHTML = "Your posts are displayd here "
+    let infoPost = document.createElement("i")
+                infoPost.className = "fa fa-spinner fa-pulse"
+                document.getElementsByClassName("posts")[0].appendChild(infoPost)
     //type 8 -load posts
     const data = {
         type: 8,
@@ -14,8 +17,8 @@ async function loadPosts()
     }
 
     let resData = await newFetch(data)
-
     let resDataPhotos = []
+
     for (let i = 0; i<resData.length; i++)
     {
         data2 = {
@@ -37,6 +40,43 @@ async function loadPosts()
                 resDataPhotos.push(userPhoto)
             else
                 resDataPhotos.push(undefined)
+
+                let post = document.createElement("div")
+                post.className = "post"
+        
+                let contact = document.createElement("div")
+                contact.className = "contact"
+                contact.style.borderBottom = "none"
+                post.appendChild(contact)
+        
+                let contactName = document.createElement("div")
+                contactName.id = "contact-name"
+                contactName.innerText = resData[i].name + " " + resData[i].surname
+                contact.appendChild(contactName)
+        
+                let contactLogin = document.createElement("div")
+                contactLogin.id = "contact-login"
+                contactLogin.innerText = resData[i].login
+                contactName.appendChild(contactLogin)
+        
+                let contactImg = document.createElement("div")
+                contactImg.id = "contact-img"
+                if (resDataPhotos[i] != undefined)
+                    contactImg.style.backgroundImage = "url("+ URL.createObjectURL(resDataPhotos[i]) +")"
+        
+                contact.appendChild(contactImg)
+        
+                contentPost = document.createElement("div")
+                contentPost.className = "content-post"
+                post.appendChild(contentPost)
+        
+                let textContentPost = document.createElement("div")
+                textContentPost.className = "text-content-post"
+                textContentPost.innerText = resData[i].text
+                contentPost.appendChild(textContentPost)
+
+                document.getElementsByClassName("posts")[0].appendChild(post)
+        
         } else {
             console.log("Ошибка HTTP: " + response.status);
         }
@@ -63,56 +103,18 @@ async function loadPosts()
                 resDataImg.push(userPhoto)
             else
                 resDataImg.push(undefined)
+
+                if (resDataImg[i] != undefined)
+                {
+                    let picContentPost = document.createElement("img")
+                    picContentPost.className="pic-content-post"
+                    picContentPost.src = URL.createObjectURL(resDataImg[i])
+                    document.getElementsByClassName("content-post")[i].appendChild(picContentPost)
+                }
         } else {
             console.log("Ошибка HTTP: " + response.status);
         }
     }
-
-    document.getElementsByClassName("posts")[0].innerHTML = "Posts are displayd here"
-    for (let i = 0; i< resData.length; i++)
-    {
-        let post = document.createElement("div")
-        post.className = "post"
-
-        let contact = document.createElement("div")
-        contact.className = "contact"
-        contact.style.borderBottom = "none"
-        post.appendChild(contact)
-
-        let contactName = document.createElement("div")
-        contactName.id = "contact-name"
-        contactName.innerText = resData[i].name + " " + resData[i].surname
-        contact.appendChild(contactName)
-
-        let contactLogin = document.createElement("div")
-        contactLogin.id = "contact-login"
-        contactLogin.innerText = resData[i].login
-        contactName.appendChild(contactLogin)
-
-        let contactImg = document.createElement("div")
-        contactImg.id = "contact-img"
-        if (resDataPhotos[i] != undefined)
-            contactImg.style.backgroundImage = "url("+ URL.createObjectURL(resDataPhotos[i]) +")"
-
-        contact.appendChild(contactImg)
-
-        let contentPost = document.createElement("div")
-        contentPost.className = "content-post"
-        post.appendChild(contentPost)
-
-        let textContentPost = document.createElement("div")
-        textContentPost.className = "text-content-post"
-        textContentPost.innerText = resData[i].text
-        contentPost.appendChild(textContentPost)
-
-        if (resDataImg[i] != undefined)
-        {
-            let picContentPost = document.createElement("img")
-            picContentPost.className="pic-content-post"
-            picContentPost.src = URL.createObjectURL(resDataImg[i])
-            contentPost.appendChild(picContentPost)
-        }
-
-        document.getElementsByClassName("posts")[0].appendChild(post)
-    }
+    
+    infoPost.style.visibility = "hidden"
 }
